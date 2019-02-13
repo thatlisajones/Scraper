718e54c3b3170ffd29924d58574c6207b14c80f4
That Lisa <thatlisajones@gmail.com>
2019-02-13 14:52:15 -0700

    Isolating elements to call

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get("/scrape", function(req, res) {
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article id").each(function(i, element) {
+    $("div.col-lg-6").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
